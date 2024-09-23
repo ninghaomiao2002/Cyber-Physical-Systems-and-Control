91205997d9fe8791588b2568968c2b3a0a7c9ac3
Ninghao Miao <ninghao.miao.2002@gmail.com>
2024-09-23 22:01:34 +0100

    Update Arduino_Code.ino

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -1,31 +1,18 @@
-//Arduino Joystick 2.0 Library, by MHeironimus (https://github.com/MHeironimus)
-//Beginners Guide, by Daniel Cantore
-//Example Code (Oct 2020), with in-depth commenting 
-
-//Initial Definitions and Setup
-//Libary Inclusion
+// include the library
 #include <Joystick.h>
 
-//Define and Allocate Input Pins to memorable names
+// define the pin connections
 #define joyX A2
 #define joyZ A3
-//#define joyZ A5
 #define joyThrottle A0
-//#define joyButton1 9åå
-//#define joyButton2 8
-//#define joyButton3 7
 
-//Initializing Axis as Integers, at a 0 default value
+// initialize the variables
 int xAxis_ = 0;
 int zAxis_ = 0;
-//int zAxis_ = 0;
 int throttle_ = 0;
 
-
+// set up functions
 Joystick_ Joystick(0x03, 0x04, 32, 2,true,false,true,false,false,false,false,true,false,false,false);
-
-//Set Auto Send State
-//Enables Auto Sending, allowing the controller to send information to the HID system, rather than waiting to be asked.
 const bool initAutoSendState = true;
 
 void setup() {
@@ -34,31 +21,30 @@ void setup() {
 
 void loop() {
   Serial.begin(9600);
-  //Axis Reading during Runtime
-  //Setting Read functions for each axis and parsing correctly. The X axis will be used as an example for explanation
-
-  //Reading the X Axis analog pin to the xAxis_ variable for processing
+  
+  // read joyX and re-map it
   xAxis_ = analogRead(joyX);
-  // Serial.println(xAxis_);
-  // xAxis_ = map(xAxis_,264,758,0,1023);
   xAxis_ = map(xAxis_,264,758,0,1023);
+
+  // send data
   Joystick.setXAxis(xAxis_);
-  Serial.println(xAxis_);
-
-
+  // Serial.println(xAxis_);
+  
+  // read joyZ and re-map it
   zAxis_ = analogRead(joyZ);
   zAxis_ = map(zAxis_,264,758,0,1023);
+
+  // send data
   Joystick.setZAxis(zAxis_);
-  //Serial.println(zAxis_);
-
+  // Serial.println(zAxis_);
   
-
+  // read joyThrottle and re-map it
   throttle_ = analogRead(joyThrottle);
-  // Serial.println(throttle_);
   throttle_ = map(throttle_,0,1023,0,511);
+
+  // send data
   Joystick.setThrottle(throttle_);
-  //Serial.println(throttle_);
+  // Serial.println(throttle_);
   
- 
 delay(10);
 }
